0269b220626fa7a725e93dec729425d48fcdffe9
João da Mata <jlfilho@users.noreply.github.com>
2024-07-02 18:40:11 -0400

    first commit

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent {
+  @Input() pensamento: any;
+
+
+
+}
